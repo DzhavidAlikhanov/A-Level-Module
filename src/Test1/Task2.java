d7f5c65c3c1d5ed6b056372839aae550266e9498
DzhavidAlikhanov <146656989+DzhavidAlikhanov@users.noreply.github.com>
2023-10-27 22:39:56 +0300

    fix_Task2

--- src/Test1/Task2.java
+++ src/Test1/Task2.java
@@ -9,29 +9,46 @@ public class Task2 {
         int chessboardSize = 8;
         int[][] chessboard = new int[chessboardSize][chessboardSize];
 
+        while (true) {
+            System.out.println("Для выхода из цикла введите число вне числового диапазона.");
+            System.out.println();
 
-        System.out.println("Введите текущее положение коня (x1 y1 - от 1 до 8): ");
-        int x1 = scanner.nextInt();
-        int y1 = scanner.nextInt();
+            System.out.println("Введите текущее положение коня (x1 y1 - от 1 до 8): ");
+            int x1 = scanner.nextInt();
+            int y1 = scanner.nextInt();
 
-        System.out.println("Введите клетку в которую вы хотите передвинуть коня (x2 y2 - от 1 до 8): ");
-        int x2 = scanner.nextInt();
-        int y2 = scanner.nextInt();
+            if (x1 < 1 || y1 < 1 || x1 > 8 || y1 > 8) {
+                System.out.println("Выход из программы.");
+                break;
+            }
 
-        boolean canMove = isKnightMovePossible(x1, y1, x2, y2, chessboardSize);
+            System.out.println("Введите клетку в которую вы хотите передвинуть коня (x2 y2 - от 1 до 8): ");
+            int x2 = scanner.nextInt();
+            int y2 = scanner.nextInt();
 
-        if (canMove) {
-            System.out.println("Конь может совершить ход. ");
-        } else {
-            System.out.println("Конь не может совершить ход. ");
+            if (x2 < 1 || y2 < 1 || x2 > 8 || y2 > 8 ) {
+                System.out.println("Выход из программы.");
+                break;
+            }
+
+
+            boolean canMove = isKnightMovePossible(x1, y1, x2, y2, chessboard);
+
+            if (canMove) {
+                System.out.println("Конь может совершить ход. ");
+            } else {
+                System.out.println("Конь не может совершить ход. ");
+            }
         }
 
         scanner.close();
     }
 
-    public static boolean isKnightMovePossible(int x1, int y1, int x2, int y2, int chessboardSize) {
-        if (x1 - 1 < 0 || x1 - 1 >= chessboardSize || y1 - 1 < 0 || y1 - 1 >= chessboardSize ||
-                x2 - 1 < 0 || x2 - 1 >= chessboardSize || y2 - 1 < 0 || y2 - 1 >= chessboardSize) {
+    public static boolean isKnightMovePossible(int x1, int y1, int x2, int y2, int[][] chessboardSize) {
+        if (x1 - 1 < 0 || x1 - 1 >= chessboardSize.length
+                || y1 - 1 < 0 || y1 - 1 >= chessboardSize.length
+                || x2 - 1 < 0 || x2 - 1 >= chessboardSize.length
+                || y2 - 1 < 0 || y2 - 1 >= chessboardSize.length) {
             return false;
         }
 
@@ -41,3 +58,4 @@ public class Task2 {
         return (deltaX == 2 && deltaY == 1) || (deltaX == 1 && deltaY == 2);
     }
 }
+
